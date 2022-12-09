0e6b91c5e8540223d596b254c16408f5d0743925
truta193 <trutaandrei703@yahoo.com>
2022-12-09 21:20:42 +0200

    Set universal style

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from pathlib import Path
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQuickControls2 import QQuickStyle
 
 
 if __name__ == "__main__":
+    QQuickStyle.setStyle("Universal")
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    qml_file = Path(__file__).resolve().parent / "main.qml"
+    qml_file = Path(__file__).resolve().parent / "qml/main.qml"
     engine.load(qml_file)
     if not engine.rootObjects():
         sys.exit(-1)
